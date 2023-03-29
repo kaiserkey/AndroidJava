d3317f1ac824c209c9c7126e31c7eb07122ee896
kaiserkey <kaiserkey2@gmail.com>
2023-03-29 01:21:32 -0300

    new changes

---
+++ Traductor/app/src/main/java/com/example/traductor/MainActivityViewModel.java
@@ -0,0 +1,17 @@
+package com.example.traductor;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    private Context context;
+
+    private 
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
