19c9e7efb24df1b59bfcc482501f2c51a8d38dd5
kaiserkey <kaiserkey2@gmail.com>
2023-03-29 04:00:47 -0300

    new changes

--- ListViewPersonalizado/app/src/main/java/com/example/listviewpersonalizado/MainActivity.java
+++ ListViewPersonalizado/app/src/main/java/com/example/listviewpersonalizado/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.listviewpersonalizado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -12,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cargarDatos();
+        generarListView();
+    }
+
+    public void generarListView() {
+        ArrayAdapter<Inmueble> adapter = new ListAdapter(this, R.layout.item, lista, getLayoutInflater());
+
+        ListView lv = findViewById(R.id.MiLista);
+        lv.setAdapter(adapter);
+
+    }
+
+    public void cargarDatos() {
+        lista.add(new Inmueble(R.drawable.img1, "Direccion 123", 180000000));
+        lista.add(new Inmueble(R.drawable.img2, "Direccion 789", 120000000));
+        lista.add(new Inmueble(R.drawable.img3, "Direccion 456", 150000000));
     }
 }
