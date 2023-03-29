91f1783a0f8dcd7de441e68e245a78317412596d
kaiserkey <kaiserkey2@gmail.com>
2023-03-29 03:38:11 -0300

    new changes

--- ListViewPersonalizado/app/src/main/java/com/example/listviewpersonalizado/ListAdapter.java
+++ ListViewPersonalizado/app/src/main/java/com/example/listviewpersonalizado/ListAdapter.java
@@ -10,7 +10,9 @@ import java.util.List;
 
 
 public class ListAdapter extends ArrayAdapter<Inmueble> {
-
+    private Context context;
+    private List<Inmueble> lista;
+    private LayoutInflater li;
     public ListAdapter(@NonNull Context context, int resource, @NonNull List<Inmueble> objects, LayoutInflater li) {
         super(context, resource, objects);
         this.conte
