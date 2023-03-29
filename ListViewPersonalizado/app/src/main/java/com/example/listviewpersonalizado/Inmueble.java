ea97231d30e32ccb37a44774223ce45c83d3037e
kaiserkey <kaiserkey2@gmail.com>
2023-03-29 03:19:34 -0300

    new changes

--- ListViewPersonalizado/app/src/main/java/com/example/listviewpersonalizado/Inmueble.java
+++ ListViewPersonalizado/app/src/main/java/com/example/listviewpersonalizado/Inmueble.java
@@ -1,2 +1,33 @@
-package com.example.listviewpersonalizado;public class Inmueble {
+package com.example.listviewpersonalizado;
+
+public class Inmueble {
+    private int foto;
+    private String direccion;
+    private double precio;
+
+    public Inmueble(int foto, String direccion, double precio) {
+        this.foto = foto;
+        this.direccion = direccion;
+        this.precio = precio;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
 }
