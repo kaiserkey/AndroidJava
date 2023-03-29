d79686e4001e440cfed3b080d6619137a6181c8c
kaiserkey <kaiserkey2@gmail.com>
2023-03-29 03:35:37 -0300

    new changes

--- ListViewPersonalizado/app/src/main/java/com/example/listviewpersonalizado/ListAdapter.java
+++ ListViewPersonalizado/app/src/main/java/com/example/listviewpersonalizado/ListAdapter.java
@@ -5,9 +5,13 @@ import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 
+import java.util.List;
+
+
 public class ListAdapter extends ArrayAdapter<Inmueble> {
 
-    public ListAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
+
+    public ListAdapter(@NonNull Context context, int resource, @NonNull List<Inmueble> objects) {
+        super(context, resource, objects);
     }
 }
