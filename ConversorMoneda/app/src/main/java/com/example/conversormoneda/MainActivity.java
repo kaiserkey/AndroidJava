1341bdcbd6f1d8830a9f9877e82d608811cde34c
kaiserkey <kaiserkey2@gmail.com>
2023-03-28 22:39:12 -0300

    new changes

--- ConversorMoneda/app/src/main/java/com/example/conversormoneda/MainActivity.java
+++ ConversorMoneda/app/src/main/java/com/example/conversormoneda/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         viewModel.getResultado().observe(this, new Observer<Double>() {
             @Override
             public void onChanged(Double convert) {
-                binding.texto3.setText(convert+"");
+                binding.textResult.setText(convert+"");
             }
         });
 
