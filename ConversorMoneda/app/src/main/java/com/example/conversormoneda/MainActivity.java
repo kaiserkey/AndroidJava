4d722d1bdb82c25a867ec5330ce82417b55d0e07
kaiserkey <kaiserkey2@gmail.com>
2023-03-28 22:25:52 -0300

    new changes

--- ConversorMoneda/app/src/main/java/com/example/conversormoneda/MainActivity.java
+++ ConversorMoneda/app/src/main/java/com/example/conversormoneda/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 if(binding.radioED.hasFocus()){
                     option = "euro";
                 }
-                viewModel.convert(binding.inputDolares.toString(),binding.inputEuros.toString(), option);
+                viewModel.calcular(binding.inputDolares.toString(),binding.inputEuros.toString(), option);
             }
         });
 
