770a3ac8cde0b5d0cf864558b996c88d7219cb76
kaiserkey <kaiserkey2@gmail.com>
2023-03-29 02:04:39 -0300

    new changes

--- Traductor/app/src/main/java/com/example/traductor/TraductorActivity.java
+++ Traductor/app/src/main/java/com/example/traductor/TraductorActivity.java
@@ -1,4 +1,17 @@
 package com.example.traductor;
 
-public class TraductorActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.traductor.databinding.ActivityTraducirBinding;
+
+public class TraductorActivity extends AppCompatActivity {
+    private ActivityTraducirBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traductor);
+    }
 }
