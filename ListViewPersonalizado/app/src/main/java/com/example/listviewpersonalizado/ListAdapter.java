c62ae22ef72b5be1be7311a9e2d43fb45d5beab1
kaiserkey <kaiserkey2@gmail.com>
2023-03-29 03:40:39 -0300

    new changes

--- ListViewPersonalizado/app/src/main/java/com/example/listviewpersonalizado/ListAdapter.java
+++ ListViewPersonalizado/app/src/main/java/com/example/listviewpersonalizado/ListAdapter.java
@@ -2,9 +2,12 @@ package com.example.listviewpersonalizado;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
@@ -13,8 +16,17 @@ public class ListAdapter extends ArrayAdapter<Inmueble> {
     private Context context;
     private List<Inmueble> lista;
     private LayoutInflater li;
+
     public ListAdapter(@NonNull Context context, int resource, @NonNull List<Inmueble> objects, LayoutInflater li) {
         super(context, resource, objects);
-        this.conte
+        this.context = context;
+        this.lista = objects;
+        this.li = li;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
     }
 }
